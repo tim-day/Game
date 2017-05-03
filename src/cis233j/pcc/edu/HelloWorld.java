e3faef7c29b17b98e5a22a37c19441994d2ffc09
Tim Day <timothy.day@pcc.edu>
2017-05-03 14:22:59 -0700

    added a main method and print statement

--- src/cis233j/pcc/edu/HelloWorld.java
+++ src/cis233j/pcc/edu/HelloWorld.java
@@ -4,4 +4,7 @@ package cis233j.pcc.edu;
  * Created by Tim on 5/3/2017.
  */
 public class HelloWorld {
+    public static void main(String[] args){
+        System.out.print("Hello World");
+    }
 }
