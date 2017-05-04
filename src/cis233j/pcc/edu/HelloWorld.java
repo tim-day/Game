be4fd53c8034789dfaa2b14d0a08108e79555b01
Tim Day <timothy.day@pcc.edu>
2017-05-04 12:02:58 -0700

    Adda a main method that prints firs mesg

--- src/cis233j/pcc/edu/HelloWorld.java
+++ src/cis233j/pcc/edu/HelloWorld.java
@@ -4,4 +4,8 @@ package cis233j.pcc.edu;
  * Created by Tim on 5/3/2017.
  */
 public class HelloWorld {
+    public static void main(String[] args)
+    {
+        System.out.println("This is the first message for branch");
+    }
 }
