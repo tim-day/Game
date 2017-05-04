d7f92376277f7fb363b24d54148c439ebd9c0f04
Tim Day <timothy.day@pcc.edu>
2017-05-04 12:11:57 -0700

    Second print statement

--- src/cis233j/pcc/edu/HelloWorld.java
+++ src/cis233j/pcc/edu/HelloWorld.java
@@ -7,5 +7,6 @@ public class HelloWorld {
     public static void main(String[] args)
     {
         System.out.println("This is the first message for branch");
+        System.out.println("This is the second message for branch");
     }
 }
